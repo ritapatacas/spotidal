0281eade07f21f6ece8486955e7e2660e781da2f
ritapatacas <ritapatacas@users.noreply.github.com>
2024-08-09 01:58:22 +0100

    organized sp_auth, add get_token and get_credentials; in sp_fetch add get_playlists_details

--- src/spotidal/sp_auth.py
+++ src/spotidal/sp_auth.py
@@ -1,35 +1,56 @@
 import os
 import sys
 import spotipy
+import spotipy.util as util
 from spotipy.oauth2 import SpotifyOAuth
-import yaml
+from dotenv import load_dotenv
 
 def get_credentials():
-    script_dir = os.path.dirname(__file__)
-    config_path = os.path.join(script_dir, '../../config.yml')
-
-    with open(config_path, 'r') as file:
-        return yaml.safe_load(file)
+    load_dotenv()
+    credentials = {
+        'username': os.getenv('SPOTIFY_CLIENT_USERNAME'),
+        'client_id': os.getenv('SPOTIFY_CLIENT_ID'),
+        'client_secret': os.getenv('SPOTIFY_CLIENT_SECRET'),
+        'redirect_uri': os.getenv('SPOTIFY_REDIRECT_URI'),
+    }
+    return credentials
 
 SPOTIFY_SCOPES = 'playlist-read-private,user-library-read'
 
 def open_session() -> spotipy.Spotify:
-    config = get_credentials()
+    credentials = get_credentials()
 
     print("\nOpening Spotify session\n")
 
     credentials_manager = SpotifyOAuth(
-        client_id=config['spotify']['client_id'],
-        client_secret=config['spotify']['client_secret'],
-        redirect_uri=config['spotify']['redirect_uri'],
+        client_id=credentials['client_id'],
+        client_secret=credentials['client_secret'],
+        redirect_uri=credentials['redirect_uri'],
         scope=SPOTIFY_SCOPES,
-        username=config['spotify']['username'],
+        username=credentials['username'],
         requests_timeout=2
     )
 
     try:
         credentials_manager.get_access_token(as_dict=False)
     except spotipy.SpotifyOauthError as e:
-        sys.exit(f"Error opening Spotify session; could not get token for username: {username}\n{str(e)}")
+        sys.exit(f"Error opening Spotify session; could not get token for username: {credentials['username']}\n{str(e)}")
 
     return spotipy.Spotify(auth_manager=credentials_manager)
+
+
+def get_token():
+    credentials = get_credentials()
+    username = credentials['username']
+    scope = 'user-library-read'
+
+    token = util.prompt_for_user_token(username, scope)
+
+    if token:
+        print("Token successfully retrieved!", token)
+    else:
+        print("Failed to retrieve token.")
+
+""" if __name__ == '__main__':
+    get_token()
+ """
--- src/spotidal/sp_fetch.py
+++ src/spotidal/sp_fetch.py
@@ -2,17 +2,20 @@ import os
 import sys
 import json
 import sp_auth as _sp_auth
+import spotipy.util as util
+
 
 
 def main():
-    spotify_session = _sp_auth.open_session()
+    session = _sp_auth.open_session()
+    playlists = get_playlists_names_ids(session)
+    #save_spotify_playlists_to_json (playlists)
+    #get_playlists_details(session)
 
-    playlists = get_spotify_playlists(spotify_session)
-    save_spotify_playlists_to_json (playlists)
 
-def get_spotify_playlists(spotify_session):
-    playlists_data = spotify_session.current_user_playlists()
-    print(f"Total Playlists: {playlists_data['total']}")
+def get_playlists_names_ids(session):
+    playlists_data = session.current_user_playlists()
+    print(f"Total Playlists: {playlists_data['total']}\n")
     playlists = [{"id": playlist['id'], "name": playlist['name'], 'sync': 'off' } for playlist in playlists_data['items']]
 
     for p in playlists:
@@ -29,6 +32,37 @@ def save_spotify_playlists_to_json(playlists):
         json.dump(playlists, file, indent=4)
 
     print("\nPlaylists saved to jsons/spotify_playlists.json")
+
+
+def show_tracks(tracks):
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
+            track['name']))
+
+
+def get_playlists_details(session):
+    username = session.current_user()['id']
+    token = util.prompt_for_user_token(username)
+
+    if token:
+        playlists = session.user_playlists(username)
+        for playlist in playlists['items']:
+            if playlist['owner']['id'] == username:
+                print()
+                print(playlist['name'])
+                print ('  total tracks', playlist['tracks']['total'])
+                results = session.playlist(playlist['id'],
+                    fields="tracks,next")
+                tracks = results['tracks']
+                show_tracks(tracks)
+                while tracks['next']:
+                    tracks = session.next(tracks)
+                    show_tracks(tracks)
+    else:
+        print("Can't get token for", username)
+
+
 
 if __name__ == '__main__':
     main()
